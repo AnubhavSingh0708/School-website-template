f4ca0851fc63b55590430a2d3c85c5fa6be5a399
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2022-12-16 17:13:20 +0530

    install button

--- script.js
+++ script.js
@@ -60,4 +60,27 @@ document.getElementById("l1").style.display="inline-block" ;
  
 }}
 
+//install
 
+let deferredPrompt;
+const addBtn = document.querySelector('.add-button');
+addBtn.style.display = 'none';
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+  addBtn.style.display = 'block';
+
+  addBtn.addEventListener('click', () => {
+    addBtn.style.display = 'none';
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the install prompt');
+      } else {
+        console.log('User dismissed the install prompt');
+      }
+      deferredPrompt = null;
+    });
+  });
+});
