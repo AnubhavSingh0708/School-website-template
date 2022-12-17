2ade7e9289a985745105b176e7882a472f529a4e
Anubhav Singh <68738228+AnubhavSingh0708@users.noreply.github.com>
2022-12-17 06:38:03 +0530

    animation on scrool

--- script.js
+++ script.js
@@ -84,3 +84,14 @@ window.addEventListener('beforeinstallprompt', (e) => {
     });
   });
 });
+const observer =new IntersectionObserver((entries)=>{
+  entries.forEach((entry)=>{
+  if (entry.isIntersecting){
+    entry.target.classList.add('show');
+  }else{
+    entry.target.classList.remove('show');
+  }
+  });
+  });
+const hiddenElements =document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=>observer.observe(el));
